76017d7740547a4e90eeec9784ad2e6e1f3bf6f4
Thiago de Oliveira Alves <towo497@gmail.com>
2020-07-21 03:00:20 -0300

    mudança do PrintSymbol e novo FileManager

---
+++ src/com/towo497/PrintSymbol.java
@@ -0,0 +1,15 @@
+package com.towo497;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class PrintSymbol {
+
+	public static void main(String[] args) throws IOException {
+		OutputStreamWriter os = new OutputStreamWriter(System.out, "UTF-8");
+		char sym = 916;
+		os.write(sym);
+
+	}
+
+}
