a75b2b424ee009bbf63797c4fe98be5e8c0cd322
Thiago de Oliveira Alves <towo497@gmail.com>
2020-08-23 01:27:10 -0300

    MyMath 1.6 imports corrigidos

--- src/com/towo497/MyMath.java
+++ src/com/towo497/MyMath.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.security.InvalidParameterException;
 import java.util.function.DoubleFunction;
 
 /**
