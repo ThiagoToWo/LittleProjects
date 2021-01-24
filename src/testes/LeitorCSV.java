2af0ae898088c9cab7ce886278e0c7a8aa0cf290
Thiago de Oliveira Alves <towo497@gmail.com>
2021-01-24 19:38:22 -0300

    mudança inútil no leitor de csv

--- src/testes/LeitorCSV.java
+++ src/testes/LeitorCSV.java
@@ -4,31 +4,37 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
-import javax.print.attribute.IntegerSyntax;
-import javax.swing.JFileChooser;
+import java.util.StringTokenizer;
 
 public class LeitorCSV {
 	
 	public static void main(String[] args) {
-		ArrayList<String[]> elementList = new ArrayList<String[]>();
-		ArrayList<ArrayList<String[]>> listaDeProvas = new ArrayList<ArrayList<String[]>>();
-		JFileChooser load = new JFileChooser();
-		load.showOpenDialog(null);
+		ArrayList<String[]> listaDeAlunos = new ArrayList<String[]>();
+		StringTokenizer stk;
+		//ArrayList<ArrayList<String[]>> listaDeArquivos = new ArrayList<ArrayList<String[]>>();
+		/*JFileChooser load = new JFileChooser();
+		load.showOpenDialog(null);*/
 		Scanner in;
 		try {
-			in = new Scanner(new BufferedReader(new FileReader(load.getSelectedFile())));
-			while (in.hasNextLine()) {
-				String line = in.nextLine();
-				String[] element = line.split(",");
-				elementList.add(element);
+			in = new Scanner(new BufferedReader(new FileReader("C:\\Users\\Thiago ToWo\\Downloads\\Registro de Frequência - 25.05.20 - Segunda.xlsx - Respostas ao formulário 1.csv")));
+			String line = in.nextLine();
+			while (in.hasNextLine()) {				
+				line = in.nextLine();
+				stk = new StringTokenizer(line, ",");
+				String[] element = new String[stk.countTokens()];
+				for (int i = 0; i < element.length; i++) {
+					element[i] = stk.nextToken();
+				}
+				listaDeAlunos.add(element);
 			}
-			listaDeProvas.add(elementList);
+			//listaDeArquivos.add(listaDeAlunos);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
-		print(elementList, 2, 8);
+		print(listaDeAlunos,6);
 	}
 	
 	private static void print(ArrayList<String[]> el, int ... is) {
@@ -40,5 +46,14 @@ public class LeitorCSV {
 		}
 	}
 	
-	
+	private static void printComCondicao(ArrayList<String[]> el, String match, int colunaMatch, int ... colunaPrint) {
+		for (String[] e : el) {
+			System.out.println(Arrays.toString(e));
+			if (e[colunaMatch].matches(match)) {
+				for (int coluna : colunaPrint) {
+					System.out.println("\t" + e[coluna]);
+				}
+			} else continue;							
+		}
+	}
 }
